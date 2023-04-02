b68f010dfc721829debed1dbe9aad28be167bbb8
qupadikkk <93931558+qupadikkk@users.noreply.github.com>
2023-04-02 18:55:40 +0300

    update

--- laba2.cs
+++ laba2.cs
@@ -67,4 +67,45 @@ namespace ConsoleApp1
                 {
                     return PriceofTicket * 1.1;
                 }
-           
+
+           public class Taxi : Transport
+            {
+                private int MaxSpeed;
+
+                public Taxi(int Gov_number, int Year_ofcar, int Num_seats, double Price_ofTicket, int Km_count, bool Is_new, int max_speed)
+                    : base(Gov_number, Year_ofcar, Num_seats, Price_ofTicket, Km_count, Is_new)
+                {
+                    MaxSpeed = max_speed;
+                    Numseats = Math.Min(4, Num_seats); 
+                }
+
+                public override string ToString()
+                {
+                    return $"Taxi: Govnumber = {Govnumber}, Yearofcar = {Yearofcar}, Numseats = {Numseats}, PriceofTicket = {PriceofTicket}, Kmcount = {Kmcount}, IsNew = {IsNew}, MaxSpeed = {MaxSpeed}";
+                }
+
+                public double CalculateTicketPrice(double distance)
+                {
+                    return distance * PriceofTicket / Numseats;
+                }
+            }
+
+            public class Bus : Transport
+            {
+                private bool HasAirConditioner;
+                
+                public Bus(int Gov_number, int Year_ofcar, int Num_seats, double Price_ofTicket, int Km_count, bool Is_new, bool has_air_conditioner)
+                    : base(Gov_number, Year_ofcar, Num_seats, Price_ofTicket, Km_count, Is_new)
+                {
+                    HasAirConditioner = has_air_conditioner;
+                }
+                public override string ToString()
+                {
+                    return $"Bus: Govnumber = {Govnumber}, Yearofcar = {Yearofcar}, Numseats = {Numseats}, PriceofTicket = {PriceofTicket}, Kmcount = {Kmcount}, IsNew = {IsNew}, HasAirConditioner = {HasAirConditioner}";
+                }
+
+                public double CalculateTicketPrice(double distance)
+                {
+                    return distance * PriceofTicket / (Numseats / 2);
+                }
+            }
